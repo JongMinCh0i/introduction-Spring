4b1c55439b9a77362d10dc8db5556fdae88f75e0
JongMinChoi <wonderland314592@gmail.com>
2022-05-19 02:09:18 +0900

    add SpringDataJpaMemberRepository

--- src/main/java/introduction/introductionSpring/SpringConfig.java
+++ src/main/java/introduction/introductionSpring/SpringConfig.java
@@ -1,35 +1,21 @@
 package introduction.introductionSpring;
 
+import introduction.introductionSpring.aop.TimeTraceAop;
 import introduction.introductionSpring.repository.*;
 import introduction.introductionSpring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-    //DataSource는 데이터베이스 커넥션을 획득할 때 사용하는 객체다. 스프링 부트는 데이터베이스 커넥션
-    //정보를 바탕으로 DataSource를 생성하고 스프링 빈으로 만들어둔다. 그래서 DI를 받을 수 있다.
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em) {
-        this.dataSource = dataSource;
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean // Bean 등록
     public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository(); // 구현체 (인터페이스를 구현한 클래스)
-//        return new JdbcMemberRepository(dataSource);
-//        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        return new MemberService(memberRepository);
     }
 }
